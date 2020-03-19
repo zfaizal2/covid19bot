2e2b1e19a85b08e3d8872dec9b81dace12b0d155
Zayyan <zfaizal2@illinois.edu>
2020-03-18 22:46:17 -0500

    gitignore fix

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-module.exports = {
-    consumer_key: 'ktaRTiwrzGNw7bPam9Ntk12nS',
-    consumer_secret: '8GAFYeFMlgitZPx6S05Bd210wjOkwPkDe8QcnBzbin2gDawQEa',
-    access_token_key: '1238708239199621122-IPRvCigKFc24UKeHf6RxjhcYa3Ks5L',
-    access_token_secret: 'GYnFprdzHLCiK2jGrHYqOHL0z6zGLMm4zVUQZZe3QmwUJ'
-  }
