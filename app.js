455a5ad6b0501116c5041e07a13c6c9cc833982f
Zayyan <zfaizal2@illinois.edu>
2020-03-20 01:36:30 -0500

    refactor

--- app.js
+++ app.js
@@ -8,13 +8,32 @@ const https = require('https')
 const request = require('request')
 
 var express = require ('express');
+let Queue = require('bull');
+
 var app = express();
 const port = 3000;
 
 
-let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 
+//worker stuff
+var Twitter = require('twitter');
+var T = new Twitter(config);
+
+// Connect to a local redis intance locally, and the Heroku-provided URL in production
+let REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
+
+// Spin up multiple processes to handle jobs to take advantage of more CPU cores
+// See: https://devcenter.heroku.com/articles/node-concurrency for more info
+let workers = process.env.WEB_CONCURRENCY || 2;
+
+// The maxium number of jobs each worker should process at once. This will need
+// to be tuned for your application. If each job is mostly waiting on network 
+// responses it can be much higher. If each job is CPU-intensive, it might need
+// to be much lower.
+let maxJobsPerWorker = 50;
 let workQueue = new Queue('work', REDIS_URL);
+
+
 
 
 app.use(bodyParser.urlencoded({ 
@@ -26,9 +45,7 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
-app.listen(process.env.PORT || 3000, function () { 
-    console.log("SERVER STARTED PORT: 3000"); 
-});
+
 
 
 
@@ -48,7 +65,7 @@ app.listen(process.env.PORT || 3000, function () {
 app.post("/hook", (req, res) => {
     console.log("hooks")
     //console.log(req.body.json) // Call your action on the request here
-    let job = await workQueue.add();
+    let job = workQueue.add();
     const site = req.body['URL']
     console.log(site)
     tweetSummary(site)
@@ -57,7 +74,7 @@ app.post("/hook", (req, res) => {
 })
 
 app.post("/replies", (req, res) => {
-    let job = await workQueue.add();
+    let job = workQueue.add();
     console.log("replies")
     //console.log(req.body.json) // Call your action on the request here
     //const sum = req.body['URL']
@@ -66,6 +83,10 @@ app.post("/replies", (req, res) => {
     res.send({"result":"success"})
     res.status(200).end() // Responding is important
 })
+
+workQueue.on('global:completed', (jobId, result) => {
+    console.log(`Job completed with result ${result}`);
+  });
 
 // function formatNumber(num) {
 //     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
@@ -90,7 +111,9 @@ app.post("/replies", (req, res) => {
 //     });
 // }
 
-
+app.listen(process.env.PORT || 3000, function () { 
+    console.log("SERVER STARTED PORT: 3000"); 
+});
 
 
 
@@ -126,4 +149,30 @@ var params = {
 //     console.log(err);
 //   }
 // })
+
+//the worker shit
+
+
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+function tweetSummary(url) {
+    console.log('hey!')
+    request(url, { json: true }, (err, res, body) => {
+
+        if (err) { 
+            return console.log(err); 
+        }
+        var sum = 0;
+        for (var i = 0; i < (body["Countries"]).length; i++) {
+            sum += body["Countries"][i]["NewConfirmed"];
+        }
+        var num = formatNumber(sum);
+        var tweet = `In the last 24 hours, there have been ${num} new confirmed cases of COVID-19.`;
+        console.log(tweet);
+        T.post('statuses/update', {status: tweet});
+        console.log(sum);
+    });
+}
 
